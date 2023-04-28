a8736ddea0891231b83a3e7a3d62881807f08d61
Marco Franco Montecillo <montecillo2030311@mls.ceu.edu.ph>
2023-04-28 22:49:24 +0800

    Revert "asdawda"
    This reverts commit d6a917b934db047d108a4908bab2efc5cf81676f.

--- resource/php/functions/funct.php
+++ resource/php/functions/funct.php
@@ -41,7 +41,7 @@ function pError($error){
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
-        </div>'; 
+        </div>';
     }
 
 function vald(){
